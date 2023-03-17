095064b4d37e0eec05c48b4cc9abd46ac0143ee1
Danh <117819805+Dhoay@users.noreply.github.com>
2023-03-17 11:40:53 +0700

    Add files via upload

---
+++ 3.2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] < arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+int main() {
+    const int MAX_SIZE = 1000;
+    int arr[MAX_SIZE];
+    int n = 0;
+
+    // Nh?p m?ng
+    while (true) {
+        int x;
+        cin >> x;
+
+        if (x == 0) {
+            break;
+        }
+
+        arr[n] = x;
+        n++;
+    }
+
+    // S?p x?p m?ng
+    bubbleSort(arr, n);
+
+    // In m?ng dã s?p x?p
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
+
